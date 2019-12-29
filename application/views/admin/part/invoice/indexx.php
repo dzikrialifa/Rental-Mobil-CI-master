3111e01051a553f3572e9f26b7e6589448d18120
dzikrialifa <dzikrialif99@gmail.com>
2019-12-29 15:39:30 +0700

    ADD INDEXX AS SECOND INDEX INVOICE

---
+++ application/views/admin/part/invoice/indexx.php
@@ -0,0 +1,51 @@
+        <!-- page content -->
+        <div class="right_col" role="main">
+
+            <div class="row">
+
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Tagihan Pending</h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+					
+                    <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+                      <thead>
+                      <button type="button" class="btn btn-default"><i class="fa fa-retweet"></i> Pending</button>
+                      <button type="button" class="btn btn-default"><i class="fa fa-check-square-o"></i> Konfirmasi</button>
+                      <button type="button" class="btn btn-success"><i class="fa fa-check"></i> Selesai</button>
+                      <button type="button" class="btn btn-danger"><i class="fa fa-close"></i> Dibatalkan</button>
+                      <div class="clearfix" style="margin-bottom:20px;"></div>
+                      <div class="alert alert-warning alert-dismissible fade in" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
+                        </button>
+                        <strong>Perhatian !</strong> Kotak pencarian digunakan untuk mencari nama pemesan dan kode tagihan
+                      </div>
+                        <tr>
+                          <th>Nama Pemesan</th>
+                          <th>Kode Tagihan</th>
+                          <th>Total Tagihan</th>
+                          <th>Tanggal</th>
+                          <th>Aksi</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <!-- <tr>
+                          <td>Michael</td>
+                          <td>Bruce</td>
+                          <td><button type="submit" class="btn btn-success"><i class="fa fa-pencil"></i></button></td>
+                        </tr> -->
+                      </tbody>
+                    </table>
+					
+					
+                  </div>
+                </div>
+              </div>
+            </div>
+         
+        </div>
+        <!-- /page content -->
+
