88384ebd052a76055b17cf6aafacee9f7043edb0
dzikrialifa <dzikrialif99@gmail.com>
2019-12-29 15:40:08 +0700

    ADD INDEXX AS SECOND INDEX TYPE

--- application/views/admin/part/type/indexx.php
+++ application/views/admin/part/type/indexx.php
@@ -6,29 +6,14 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Tipe Kendaraan</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <ul class="dropdown-menu" role="menu">
-                          <li><a href="#">Settings 1</a>
-                          </li>
-                          <li><a href="#">Settings 2</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
+                    <h2>Nama Tipe Kendaraan</h2>
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
 					
                     <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
                       <thead>
-                      <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">Large modal</button>
+                      <button type="button" class="btn btn-default" data-toggle="modal" data-target=".bs-example-modal-md">Tambah Kendaraan</button>
 
                         <tr>
                           <th>Tipe Kendaraan</th>
@@ -38,9 +23,11 @@
                       </thead>
                       <tbody>
                         <tr>
-                          <td>Michael</td>
-                          <td>Bruce</td>
-                          <td><button type="submit" class="btn btn-success"><i class="fa fa-pencil"></i></button></td>
+                          <td>Fortuner</td>
+                          <td>2019-05-21</td>
+                          <td>
+                          <button type="submit" class="btn btn-success"><i class="fa fa-pencil"></i></button>
+                          <button type="submit" class="btn btn-danger"><i class="fa fa-trash"></i></button></td>
                         </tr>
                       </tbody>
                     </table>
@@ -55,38 +42,36 @@
         <!-- /page content -->
         <!-- Large modal -->
                   
-                  <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
-                    <div class="modal-dialog modal-lg">
+                  <div class="modal fade bs-example-modal-md" tabindex="-1" role="dialog" aria-hidden="true">
+                    <div class="modal-dialog modal-md">
                       <div class="modal-content">
 
                         <div class="modal-header">
                           <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
                           </button>
-                          <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                          <h4 class="modal-title" id="myModalLabel">Tambah Nama Tipe Kendaraan</h4>
                         </div>
                         <div class="modal-body">
                           <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
 
                       <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Nama Kendaraan <span class="required">*</span>
+                        <label for="first-name">Nama Tipe Kendaraan 
                         </label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                          <input type="text" id="first-name" required="required" class="form-control col-md-7 col-xs-12">
-                        </div>
+                        
+                          <input type="text" id="first-name" class="form-control" required="required" placeholder="nama tipe kendaraan">
+                        
                       </div>
                       
                       <div class="form-group">
-                        <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Deskripsi</label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                          <input id="middle-name" class="form-control col-md-7 col-xs-12" type="text" name="middle-name">
-                        </div>
+                        <label for="middle-name">Deskripsi</label>
+                        
+                          <input id="middle-name" class="form-control" type="text" name="middle-name" placeholder="deskripsi tipe kendaraan">
+                        
                       </div>
-                      <div class="ln_solid"></div>
                       <div class="form-group">
                         <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                          <button class="btn btn-primary" type="button">Cancel</button>
-						  <button class="btn btn-primary" type="reset">Reset</button>
-                          <button type="submit" class="btn btn-success">Submit</button>
+                          <button class="btn btn-danger" data-dismiss="modal"  type="button">Cancel</button>
+                          <button type="submit" class="btn btn-primary">Simpan</button>
                         </div>
                       </div>
 
